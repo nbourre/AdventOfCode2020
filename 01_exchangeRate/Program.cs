bec00bb57350db13489271445004d2ec57602a76
Nick B <nbourrereg@hotmail.com>
2020-12-02 10:12:23 -0500

    Puzzle 2 solved

--- 01_exchangeRate/Program.cs
+++ 01_exchangeRate/Program.cs
@@ -7,19 +7,20 @@ namespace _01_exchangeRate
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Answer : 215 * 1805 = 388075
+        /// </summary>
+        /// <param name="content"></param>
+        static void PartA(string[] content)
         {
-            var filename = "01_input.txt";
-            var fileContent = File.ReadAllLines(filename);
-
-            var values = fileContent.Select(Int32.Parse).ToList();
+            var values = content.Select(Int32.Parse).ToList();
 
             int goal = 2020;
 
             int opA = 0;
             int opB = 0;
 
-            
+
             foreach (var val in values)
             {
                 var delta = goal - val;
@@ -45,6 +46,81 @@ namespace _01_exchangeRate
             Console.WriteLine($"Operand A : {opA}");
             Console.WriteLine($"Operand B : {opB}");
             Console.WriteLine($"Result of opA * opB = {opA * opB}");
+        }
+
+        /// <summary> 
+        /// Answer : 558 * 823 * 639 = 293450526
+        /// </summary>
+        /// <param name="content"></param>
+        static void PartB(string[] content)
+        {
+            var values = content.Select(Int32.Parse).ToList();
+
+            int goal = 2020;
+
+            // Les opérandes
+            int opA = 0;
+            int opB = 0;
+            int opC = 0;
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                var v1 = values[i];
+
+                opA = v1;
+
+                var delta1 = goal - opA;
+
+                opB = 0;
+                for (int j = i + 1; j < values.Count; j++)
+                {
+                    var v2 = values[j];
+
+                    /// Si la valeur est plus grande
+                    /// que le delta, ça ne sert à rien
+                    /// de continuer avec cette valeur
+                    /// on va toujours dépasser l'objectif sinon
+                    if (v2 >= delta1) continue;
+
+                    opB = v2;
+
+                    var delta2 = goal - opA - opB;
+
+                    opC = 0;
+                    for (int k = j + 1; k < values.Count; k++)
+                    {
+                        var v3 = values[k];
+
+                        if (v3 == delta2)
+                        {
+                            /// On vient de trouver un triplet qui est bon
+                            opC = v3;
+                            break;
+                        }
+                    }
+
+                    if (opC > 0)
+                        break;
+                }
+
+                if (opC > 0)
+                    break;
+            }
+
+            Console.WriteLine($"Operand A : {opA}");
+            Console.WriteLine($"Operand B : {opB}");
+            Console.WriteLine($"Operand C : {opC}");
+            Console.WriteLine($"Sum: {opA + opB + opC}");
+            Console.WriteLine($"Mult: {opA * opB * opC}");
+        }
+
+        static void Main(string[] args)
+        {
+            var filename = "01_input.txt";
+            var fileContent = File.ReadAllLines(filename);
+
+            PartA(fileContent);
+            PartB(fileContent);            
         }
     }
 }
---
+++ 02_password_check/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02_password_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "02_input.txt";
+            var fileContent = File.ReadAllLines(filename);
+
+            PartA(fileContent);
+            PartB(fileContent);
+        }
+
+        private static void PartA(string[] fileContent)
+        {
+            int count = 0;
+            foreach(var line in fileContent)
+            {
+                count += IsValidPasswordA(line) ? 1 : 0;
+            }
+
+
+            Console.WriteLine($"A. Nombre de mot de passe valide : {count}");
+        }
+
+        private static void PartB(string[] fileContent)
+        {
+            int count = 0;
+            foreach (var line in fileContent)
+            {
+                count += IsValidPasswordB(line) ? 1 : 0;
+            }
+
+
+            Console.WriteLine($"B. Nombre de mot de passe valide : {count}");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="line">Format : 0-1 l: pw. 0 <= min occurence, 1 <= max occurence, pw <= password</param>
+        /// <returns></returns>
+        private static bool IsValidPasswordA (string line)
+        {
+            bool result = false;
+            var splitted = line.Split(":");
+
+            string rule = splitted[0];
+            string password = splitted[1].Trim();
+
+            Regex ruleCheck = new Regex(@"(\d{1,2})-(\d{1,2}) (\w)") ;
+
+            var match = ruleCheck.Match(rule);
+
+            var min = int.Parse(match.Groups[1].Value);
+            var max = int.Parse(match.Groups[2].Value);
+            var letter = match.Groups[3].Value.ToCharArray()[0];
+
+            int count = password.Count(c => c == letter);
+
+            result = count >= min && count <= max;
+
+            return result;
+        }
+
+        private static bool IsValidPasswordB(string line)
+        {
+            bool result = false;
+            var splitted = line.Split(":");
+
+            string rule = splitted[0];
+            string password = splitted[1]; // No trim must keep space
+
+            Regex ruleCheck = new Regex(@"(\d{1,2})-(\d{1,2}) (\w)");
+
+            var match = ruleCheck.Match(rule);
+
+            var posA = int.Parse(match.Groups[1].Value);
+            var posB = int.Parse(match.Groups[2].Value);
+            var letter = match.Groups[3].Value.ToCharArray()[0];
+
+            // Ou exclusif (XOR)
+            result = password[posA] == letter ^ password[posB] == letter;
+
+
+            return result;
+        }
+    }
+}
