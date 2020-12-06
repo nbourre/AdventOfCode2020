ef25ecfbc9ff1cd124ac4e7d8ffa19884f01d9b9
Nick B <nbourrereg@hotmail.com>
2020-12-06 15:53:24 -0500

    Day 06a done

---
+++ 06_custom_customs/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _06_custom_customs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "input.txt";
+            var fileContent = File.ReadAllLines(filename);
+
+            DoIt(fileContent);
+        }
+
+        private static void DoIt(string[] fileContent)
+        {
+            List<List<string>> groups = new List<List<string>>();
+
+            List<string> group = new List<string>();
+            int lineCount = 0;
+
+            /// Grouping
+            foreach (var line in fileContent)
+            {
+                if (line != "")
+                {
+                    group.Add(line);
+                }
+
+                if (line == string.Empty || lineCount == fileContent.Length - 1)
+                {
+                    groups.Add(group);
+
+                    group = new List<string>();
+                }
+
+                lineCount++;
+            }
+
+
+            int result = 0;
+
+            /// Individual
+            /// 
+            foreach (var gang in groups)
+            {
+                result += GetGroupResult(gang);
+            }
+
+            Console.WriteLine($"The result is {result}");
+            
+        }
+
+        private static int GetGroupResult(List<string> gang)
+        {
+            int result = 0;
+
+            result = gang.Aggregate((i, j) => i + j).Distinct().Count();
+
+            return result;
+        }
+    }
+}
